56109f0b13c38884dff9707118332a54bd8fff48
mooncrown04 <mooncrown04@gmail.com>
2026-04-04 00:51:26 +0300

    Update m3u_sinema.py

--- m3u_sinema.py
+++ m3u_sinema.py
@@ -7,9 +7,9 @@ from collections import Counter
 
 # --- AYARLAR ---
 m3u_sources = [
-    ("https://raw.githubusercontent.com/Lunedor/iptvTR/refs/heads/main/FilmArsiv.m3u", "Lunedor"),
-    ("https://raw.githubusercontent.com/Zerk1903/zerkfilm/refs/heads/main/Filmler.m3u", "Zerk"),
     ("https://tinyurl.com/2ao2rans", "powerboard"),
+    ("https://raw.githubusercontent.com/Zerk1903/zerkfilm/refs/heads/main/Filmler.m3u", "Zerk"),
+    ("https://raw.githubusercontent.com/Lunedor/iptvTR/refs/heads/main/FilmArsiv.m3u", "Lunedor"),    
 ]
 
 birlesik_dosya = "birlesik_sinema.m3u"
