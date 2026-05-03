8f452bd286d2199a37e7c14d634e8c4f9c6eedd6
mooncrown04 <mooncrown04@gmail.com>
2026-05-03 21:12:44 +0300

    Update filtrelisinema.js

--- filtrelisinema.js
+++ filtrelisinema.js
@@ -1,23 +1,84 @@
 #EXTM3U
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.life/v/25097/poster/thumb/hep-yek.jpg" group-title="TÜRK FİLMİ" year="2013" ,Hep Yek
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/25097/poster/thumb/hep-yek.jpg" group-title="TÜRK FİLMİ SERİ" year="2013" ,Hep Yek
 https://p3.photofunny.org/v/ojx9ybe2jkys/master.m3u8
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/hep-yek-1-73108.jpg" group-title="TÜRK FİLMİ"year="2013",Hep Yek
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/hep-yek-1-73108.jpg" group-title="TÜRK FİLMİ SERİ"year="2013",Hep Yek
 https://s20.imagescdns.pics/m1/FTIjYyyynl4lZQR2YyqSDv1RGP4kZQtjpNd0zxnJ1uM2ImL2Ehpl5jnJAms0xi20vr1
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/hep-yek-2-91137.jpg" group-title="TÜRK FİLMİ" year="2017" ,Hep Yek 2
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/hep-yek-2-91137.jpg" group-title="TÜRK FİLMİ SERİ" year="2017" ,Hep Yek 2
 https://s20.imagebin.pics/md/FTIjYyyynl4lYwVjZGphI0IPYHEZYwRjBQOjd0zxnJ1uM2IvnJ4hpTywpjs0xi20vr1
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Zerk]" tvg-logo="https://www.hdfilmizle.life/v/25516/poster/thumb/hep-yek-2.jpg" group-title="TÜRK FİLMİ" year="2017" ,Hep Yek 2
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/25516/poster/thumb/hep-yek-2.jpg" group-title="TÜRK FİLMİ SERİ" year="2017" ,Hep Yek 2
 https://p3.photofunny.org/v/8dhtgrvtagkq/master.m3u8
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/hep-yek-3-titrettin-beni-38537.jpg" group-title="TÜRK FİLMİ" year="2019" ,Hep Yek 3 Titrettin Beni
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/hep-yek-3-titrettin-beni-38537.jpg" group-title="TÜRK FİLMİ SERİ" year="2019" ,Hep Yek 3 Titrettin Beni
 https://s20.imageshub.pro/mj/FTIjYyyynl4mYyEcqUWyqUEcov5PMJ5cYwVjZGxhI0IPYHEZYwRjBQOjd0zxnJ1uM2ImnUIvYaOlojs0xi20vr1
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Zerk]" tvg-logo="https://www.hdfilmizle.life/v/25065/poster/thumb/hep-yek-3-titrettin-beni.jpg" group-title="TÜRK FİLMİ" year="2019" ,Hep Yek 3 Titrettin Beni
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/25065/poster/thumb/hep-yek-3-titrettin-beni.jpg" group-title="TÜRK FİLMİ SERİ" year="2019" ,Hep Yek 3 Titrettin Beni
 https://p2.photofunia.pro/v/69j734d0y7um/master.m3u8
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/hep-yek-4-bela-okuma-altan-3971.jpg" group-title="TÜRK FİLMİ" year="2021" ,Hep Yek 4 Bela Okuma Altan
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/hep-yek-4-bela-okuma-altan-3971.jpg" group-title="TÜRK FİLMİ SERİ" year="2021" ,Hep Yek 4 Bela Okuma Altan
 https://s20.imagebin.pics/md/FTIjYyyynl40YxWyoTRhG2g1oJRhDJk0LJ4hZwNlZF5KEHVgERjhZGN4ZUNd0zxnJ1uM2IvnJ4hpTywpjs0xi20vr1
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.life/v/54738/poster/thumb/hep-yek-5-bizim-seyimiz-mi-altan.jpg" group-title="TÜRK FİLMİ" year="2022" ,Hep Yek 5 Bizim Şeyimiz mi Altan
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/54738/poster/thumb/hep-yek-5-bizim-seyimiz-mi-altan.jpg" group-title="TÜRK FİLMİ SERİ" year="2022" ,Hep Yek 5 Bizim Şeyimiz mi Altan
 https://vs5.photour.org/v/g9nj3oul24te/master.m3u8
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.life/v/54736/poster/thumb/hep-yek-dugun.jpg" group-title="TÜRK FİLMİ" year="2023" ,Hep Yek Düğün
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/54736/poster/thumb/hep-yek-dugun.jpg" group-title="TÜRK FİLMİ SERİ" year="2023" ,Hep Yek Düğün
 https://vs1.photour.org/v/oig62e6itr6a/master.m3u8
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.life/v/58868/poster/thumb/hep-yek-asiret-izle.jpg" group-title="TÜRK FİLMİ" year="2024" ,Hep Yek Aşiret
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/58868/poster/thumb/hep-yek-asiret-izle.jpg" group-title="TÜRK FİLMİ SERİ" year="2024" ,Hep Yek Aşiret
 https://vs1.photour.org/v/md4upqq9hcot/master.m3u8
-#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.life/v/502901/poster/thumb/hep-yek-loto.jpg" group-title="TÜRK FİLMİ" year="2025" ,Hep Yek Loto
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/502901/poster/thumb/hep-yek-loto.jpg" group-title="TÜRK FİLMİ SERİ" year="2025" ,Hep Yek Loto
 https://vs14.photoflick.org/v/Hep.Yek.Loto.2025.WEB-DL.1080p.H.264-HDM/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/18038/poster/thumb/taxi.jpg" group-title="SERI FILM" year="2004" , Taxi
+https://p2.photofunia.pro/v/tlxtdsgapx9s/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/32771/poster/thumb/taksi.jpg" group-title="SERI FILM" year="1998" ,Taksi
+https://p2.photofunia.pro/v/usqy3596pr3m/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/33204/poster/thumb/taksi-2.jpg" group-title="SERI FILM" year="2000" ,Taksi 2
+https://p3.photostack.net/v/8yptlarbjxrf/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="" group-title="SERI FILM" year="2000",Taxi 2
+http://movie.okru.workers.dev/?ID=7403749583240
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/33200/poster/thumb/taksi-3.jpg" group-title="SERI FILM" year="2003" ,Taksi 3
+https://p1.photostack.net/v/aabt2g3p396r/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/taksi-3-taxi-3-76864.jpg" group-title="SERI FILM" year="2003" ,Taksi 3
+https://s16.photogrids.site/mf/ITS4nF4mYwVjZQZhDzk1pzS5YwRjBQOjYxEIDHjhZtd0zxpTuiqT9apzyxpl5mnKEys0xi16vr1
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [powerboard]" tvg-logo="https://upload.wikimedia.org/wikipedia/tr/d/d7/Taksi_4.jpg" group-title="SERI FILM" year="2007",Taxi 4
+https://s15.imglink.pro/mu/ITS4nF40YwVjZQphDzk1pzS5YwRjBQOjYxEIDHjd0zxnJ1aoTyhnl5jpz8s0xi15vr1
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/33196/poster/thumb/taksi-4.jpg" group-title="SERI FILM",Taksi 4
+https://vs4.photour.org/v/6gqqsxnos3cj/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/19141/poster/thumb/taksi-5.jpg" group-title="SERI FILM",Taksi 5
+https://p1.photofunia.pro/v/1d1j9u4acykd/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/14717/poster/thumb/yaratik.jpg" group-title="SERI FILM Alien",Yaratık
+https://p3.photofunia.pro/v/om714rvr76cq/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/16400/poster/thumb/yaratigin-donusu.jpg" group-title="SERI FILM Alien" year="1986",Yaratık: Yaratığın Dönüşü
+https://p1.photofunny.org/v/x6vv18zph3q3/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/16609/poster/thumb/yaratik-3.jpg" group-title="SERI FILM Alien" year="1992" ,Yaratık 3
+https://p1.photofunia.pro/v/hl1yqrksq9xi/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/27701/poster/thumb/yaratik-dirilis.jpg" group-title="SERI FILM Alien" year="1997" ,Yaratık: Diriliş
+https://p1.photostack.net/v/55twdp1a04as/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [powerboard]" tvg-logo="https://upload.wikimedia.org/wikipedia/tr/b/bb/Yaratık_Antlaşma_-_Alien_Covenant_-_Afiş.jpg" group-title="SERI FILM Alien" year="2017" ,Alien: Covenant
+https://s6.imglink.pro/mt/DJkcMJ4hD292MJ5uoaDhZwNkAl4kZQtjpP5PoUIFLKxhESIOGNd0zxnJ1aoTyhnl5jpz8s0xi6vr1main/index.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/fullhd-yaratik-covenant-alien-covenant.jpg" group-title="SERI FILM Alien" year="2017" ,Alien: Covenant
+https://s6.imglink.pro/mt/DJkcMJ4hD292MJ5uoaDhZwNkAl4kZQtjpP5PoUIFLKxhESIOGNd0zxnJ1aoTyhnl5jpz8s0xi6vr1
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [powerboard]" tvg-logo="https://images.justwatch.com/poster/317199560/s718/alien-romulus.jpg" group-title="SERI FILM Alien" year="2024" ,Alien: Romulus
+https://s10.picogallery.org/mb/DJkcMJ4hHz9gqJk1pl4lZQV0YyqSDv1RGP4kZQtjpP5RIHSZd0zxpTywo2quoTkypaxho3Was0xi10vr1main/index.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Lunedor]" tvg-logo="https://img.fullhdfilmizlesene.de/poster/film/alien-romulus-2-79752.jpg" group-title="SERI FILM Alien" year="2024" ,Alien: Romulus
+https://s10.picogallery.org/mb/DJkcMJ4hHz9gqJk1pl4lZQV0YyqSDv1RGP4kZQtjpP5RIHSZd0zxpTywo2quoTkypaxho3Was0xi10vr1
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/58367/poster/thumb/alien-romulus-hd.jpg" group-title="SERI FILM Alien" year="2024" ,Alien: Romulus
+https://vs9.photofunny.org/v/Alien.Romulus.2024.WEB-DL.1080p.DUAL/master.m3u8
+
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/17471/poster/thumb/alien-predatora-karsi.jpg" group-title="SERI FILM AVP" year="2004" ,AVP: Alien vs. Predator
+https://p1.photostack.net/v/5apwxgs5q1pa/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/28740/poster/thumb/aliens-vs-predator-requiem.jpg" group-title="SERI FILM AVP" year="2007" ,Alien Predator'e Karşı 2: Requiem 
+https://p2.photostack.net/v/71mqqncq3z2k/master.m3u8
+
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/13586/poster/thumb/predators.jpg" group-title="SERI FILM Predator" year="2010",Predators
+https://p3.photostack.net/v/v3zmkhfwkw0b/master.m3u8
+
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/503278/poster/thumb/predator-vahsi-topraklar.jpg" group-title="SERI FILM Predator" year="2025" ,Predator: Badlands - Predator Vahşi Topraklar
+https://vs21.gamephotos.pro/v/Predator.Badlands.2025.FHD/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/503309/poster/thumb/predator-wastelands.jpg" group-title="SERI FILM Predator" year="2025" ,Predator Wastelands
+https://vs14.photoflix.org/v/Predator.Wastelands.2025.1080p.WEBRip/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/13591/poster/thumb/predator.jpg" group-title="SERI FILM Predator" year="2018" ,The Predator
+https://p3.photofunia.pro/v/iqvmr1zwya13/master.m3u8
+#EXTINF:-1 type="video" group-time="2026-04-05_14:10:19" group-author=" 🔄 [Zerk]" tvg-logo="https://www.hdfilmizle.to/v/502083/poster/thumb/predator-killer-of-killers.jpg" group-title="Aksiyon",Predator: Killer of Killers
+https://vs10.photostack.net/v/Predator.Killer.of.Killers.2025.WEB-DL.1080p.DUAL.H.264-HDM/master.m3u8
+
+
+
+
+
+
+
+
