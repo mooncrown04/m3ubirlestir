4607d66e1dc331b5408c1c07528b760d6028bfb2
mooncrown04 <mooncrown04@gmail.com>
2026-08-19 23:36:24 +0300

    Update m3u_tv.py

--- m3u_tv.py
+++ m3u_tv.py
@@ -59,6 +59,7 @@ CHANNEL_NAME_MAPPING = {
 # --- 4. KAYNAKLAR (URL ve Yazar Bilgisi) ---
 # Format: (URL, YAZAR_ISMI)
 M3U_SOURCES = [
+    ('https://raw.githubusercontent.com/efendikral54-max/M3u-Listem/refs/heads/main/IPTVSevenler.m3u', "IPTVSevenler"),
     ('https://raw.githubusercontent.com/smartgmr/cdn/refs/heads/main/Perfect.m3u', "smartgmr"),
     ('https://raw.githubusercontent.com/Mertcantv/Mertcan/refs/heads/main/%C4%B0zle2.m3u', "Mertcantv"),
     ('https://raw.githubusercontent.com/primatzeka/kurbaga/main/NeonSpor/NeonSpor.m3u', "NeonSpor"),
